926a58fad77ad9130299a4ed900a5042e5215f6d
DeltaF1 <delyaerricson@gmail.com>
2015-07-20 23:14:06 -0400

    Started prototyping solar sail mechanic

--- src/game.lua
+++ src/game.lua
@@ -2,12 +2,63 @@ local game = {}
 
 INITIAL_STATE = "game"
 
-function game.load()
+sun = {
+	pos = Vector2(50,100),
+	radius = 100,
+}
+
+function sun:draw()
+	love.graphics.setColor(200,112,0)
+	love.graphics.circle("fill", self.pos.x, self.pos.y, self.radius, 100)
+end
+
+player =
+{
+	pos = Vector2(200,200),
+	acceleration = Vector2(),
+	vel = Vector2(),
+	dir = Vector2(),
+	rot=0
+}
+
+function player:update(dt)
+	self.dir = Vector2(math.cos(self.rot), math.sin(self.rot))
+	-- Replace with an Entity super class that does this automatically? Entity.update(self, dt)?
+	local newVel = self.vel + self.acceleration  * dt
+	self.vel = newVel 
+	self.pos = self.pos + self.vel * dt
+	
+	dis = self.pos - sun.pos
+	theta = math.atan2(dis.y, dis.x) - self.rot
+	
+	falloff = 1 / math.pow((dis:len() - sun.radius), 1)
+	
+	self.acceleration = self.acceleration + self.dir:norm() * math.cos(theta) * falloff
+end
+
+function player:draw()
+	love.graphics.setColor(255,255,255)
+	love.graphics.line(self.pos.x, self.pos.y, self.pos.x+self.dir.x*10, self.pos.y+self.dir.y*10)
+end
+
+
+
+function game:load()
 
 end
 
-function game.update()
+function game:update(dt)
+	if love.keyboard.isDown("a") then
+		player.rot = player.rot + dt
+	elseif love.keyboard.isDown("d") then
+		player.rot = player.rot - dt
+	end
+	player:update(dt)
+end
 
+function game:draw()
+	sun:draw()
+	player:draw()
 end
 
 function gravity(affectedByGravity, gravityAffectors, dt)
