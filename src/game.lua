bc561d72e2cb1c9e546c09effa7f8676e58acecd
DeltaF1 <delyaerricson@gmail.com>
2015-07-21 23:18:31 -0400

    Implemented Entity system and gravity

--- src/game.lua
+++ src/game.lua
@@ -2,6 +2,35 @@ local game = {}
 
 INITIAL_STATE = "game"
 
+Entity = Class{}
+
+function Entity:init(pos, vel, accel)
+	self.pos = pos or Vector2()
+	self.vel = vel or Vector2()
+	self.accel = accel or Vector2()
+	self.netForce = Vector2()
+end
+
+function Entity:applyForce(f)
+	self.netForce = self.netForce + f
+end
+
+function Entity:update(dt)
+	self.accel = self.netForce -- Divide by mass?
+	
+	self.vel = self.vel + self.accel * dt
+	self.pos = self.pos + self.vel * dt
+	
+	self.netForce = Vector2()
+end
+
+Asteroid = Class{__includes = Entity}
+
+function Asteroid:draw()
+	love.graphics.setColor(100,100,100)
+	love.graphics.circle("fill", self.pos.x, self.pos.y, 10, 8)
+end
+
 sun = {
 	pos = Vector2(50,100),
 	radius = 100,
@@ -12,39 +41,37 @@ function sun:draw()
 	love.graphics.circle("fill", self.pos.x, self.pos.y, self.radius, 100)
 end
 
-player =
-{
-	pos = Vector2(200,200),
-	acceleration = Vector2(),
-	vel = Vector2(),
-	dir = Vector2(),
-	rot=0
-}
+player = Entity(Vector2(200,200))
+player.dir = Vector2()
+player.rot = 0
+
 
 function player:update(dt)
 	self.dir = Vector2(math.cos(self.rot), math.sin(self.rot))
-	-- Replace with an Entity super class that does this automatically? Entity.update(self, dt)?
-	local newVel = self.vel + self.acceleration  * dt
-	self.vel = newVel 
-	self.pos = self.pos + self.vel * dt
 	
 	dis = self.pos - sun.pos
 	theta = math.atan2(dis.y, dis.x) - self.rot
 	
-	falloff = 1 / math.pow((dis:len() - sun.radius), 1)
+	falloff = 1 --/ math.pow((dis:len() - sun.radius), 1)
 	
-	self.acceleration = self.acceleration + self.dir:norm() * math.cos(theta) * falloff
+	self:applyForce(self.dir:norm() * math.cos(theta) * falloff)
+	
+	Entity.update(self, dt)
 end
+
 
 function player:draw()
 	love.graphics.setColor(255,255,255)
 	love.graphics.line(self.pos.x, self.pos.y, self.pos.x+self.dir.x*10, self.pos.y+self.dir.y*10)
+	love.graphics.setPointSize(5)
+	love.graphics.point(self.pos.x, self.pos.y)
 end
 
 
 
 function game:load()
-
+	self.asteroids = List()
+	self.planets = List()
 end
 
 function game:update(dt)
@@ -53,36 +80,57 @@ function game:update(dt)
 	elseif love.keyboard.isDown("d") then
 		player.rot = player.rot - dt
 	end
+	
+	local affected = {player, unpack(self.asteroids.items)}
+	
+	gravity(affected, self.planets.items, dt)
+	
 	player:update(dt)
+	self.asteroids:update(dt)
 end
 
 function game:draw()
 	sun:draw()
 	player:draw()
+	self.asteroids:draw()
+	self.planets:draw()
+end
+
+function game:mousepressed(x, y, button)
+	if button == "l" then
+		self.asteroids:add(Asteroid(Vector2(x,y)))
+	elseif button == "r" then
+		local r = math.random(50,150)
+		self.planets:add(Planet(Vector2(x,y), r, r/200))
+	end
 end
 
 function gravity(affectedByGravity, gravityAffectors, dt)
 	for i, affected in ipairs(affectedByGravity) do
 		netForce = Vector2(0,0)
 		for x, affector in ipairs(gravityAffectors) do
-			delta = affected.pos - affector.pos
+			assert(affected.pos, "affected has no pos!")
+			assert(affector.pos, "affector has no pos!")
+			delta = affector.pos - affected.pos
 			dis = delta:len() * delta:len()
 			falloff = (dis / affector.radius)
 			netForce = netForce + (delta:norm() * affector.gravityForce * falloff)
 		end
-		affected.acceleration = affected.acceleration + netForce * dt
+		affected:applyForce(netForce)
 	end
 end
 
 Planet = Class{}
-function Planet:init(self, pos, radius, gravityForce)
+
+function Planet:init(pos, radius, gravityForce)
 	self.pos = pos
 	self.radius = radius
 	self.gravityForce = gravityForce
+	self.colour = {math.random(0, 255), math.random(0, 255), math.random(0, 255)}
 end
 
 function Planet:draw()
-	love.graphics.setColor(math.random(0, 255), math.random(0, 255), math.random(0, 255))
+	love.graphics.setColor(self.colour)
 	love.graphics.circle("fill", self.pos.x, self.pos.y, self.radius, 100)
 end
 
