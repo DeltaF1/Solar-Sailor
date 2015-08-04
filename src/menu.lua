60b918103fa5dd1dc7dc85f25a9d24c46086268d
DeltaF1 <delyaerricson@gmail.com>
2015-08-03 23:04:33 -0400

    Implemented menu

--- src/menu.lua
+++ src/menu.lua
@@ -2,6 +2,17 @@ local menu = {}
 
 INITIAL_STATE = "menu"
 
+local menuButton = Class{__includes=Button}
+
+menuButton.defaultOptions = update(copy(Button.defaultOptions), {
+	font = love.graphics.newFont("assets/fonts/Minecraftia.ttf", 44),
+	colours = {
+		default = {54,107,100},
+		over = {68,183,168},
+		down = {35,70,65},
+	},
+})
+
 function menu:load()
 	
 	--self.camera = 
@@ -11,9 +22,18 @@ function menu:load()
 	
 	self.frame = Frame(Vector2(), Vector2(width,height))
 	
-	local startbutton = self.frame:add(Button(Vector2(), Vector2(200,50), {onClick = function() EndState("game", "new") end}))
-	  :center()
-	startbutton.texts.default="Start"
+	self.frame.draw = function() end
+	
+	local title = self.frame:add(Label("SOLAR SAILOR", Vector2(0, 25), nil, {font=love.graphics.newFont("assets/fonts/Minecraftia.ttf", 92)})):centerX()
+	
+	local startbutton = self.frame:add(menuButton(Vector2(0,195), Vector2(400,85), {onClick = function() EndState("game", "new") end}))
+	startbutton.texts.default="Start Game"
+	
+	local creditsbutton = self.frame:add(menuButton(Vector2(0, 424), Vector2(283, 85), {onClick = function() EndState("credits") end}))
+	creditsbutton.texts.default = "Credits"
+	
+	local exitbutton = self.frame:add(menuButton(Vector2(0, 652), Vector2(187, 85), {onClick = function() love.event.quit() end}))
+	exitbutton.texts.default = "Exit"
 	
 	local socialFrame = self.frame:add(
 		Frame(Vector2(), Vector2(150,200))
@@ -45,6 +65,12 @@ function menu:load()
 	end
 	
 	--]]
+	
+	self.player = love.graphics.newImage("assets/img/player.png")
+	self.playerPos = Vector2(700, 500)
+	self.ox, self.oy = self.player:getWidth()/2, self.player:getHeight()/2
+	self.fac = 0.01
+	
 	self.gui = List{self.frame}
 	self.gui:add(self.frame.children)
 	self.gui:add(socialFrame.children)
@@ -56,9 +82,14 @@ function menu:load()
 end
 
 function menu:draw()
-	--self.camera:draw(function(l,t,w,h)
+	
+	--draw stars
+	
+	
+	
+	local x,y = love.mouse.getX(), love.mouse.getY()
+	love.graphics.draw(self.player, self.playerPos.x+(x*self.fac), self.playerPos.y+(y*self.fac), 0, 10, nil, self.ox, self.oy)
 	self.gui:draw()
-	--end)
 end
 
 function menu:update(dt)
